7d2f125c4369485abe0af5107a297a31c890accc
Stephan Thober <stephan.thober@ufz.de>
2015-09-24 08:45:08 +0000

    trunk: pre-proc: added calculation of latitude and longitude at level 0 in create_latlon.py

--- pre-proc/create_latlon.py
+++ pre-proc/create_latlon.py
@@ -11,7 +11,74 @@
 #                                     create 1 dimensional xx array now with linspace, which is cleaner and solves a bug in the creation of the y direction
 #
 #############################################
-#
+
+
+def header_to_latlon(headerfile, coord_sys):
+    # This function returns the latitude longitude given a ASCII header file
+
+    # check input files
+    if not os.path.isfile(headerfile):
+      sys.exit("No "+headerfile+" file found here, are you in the right directory?")
+
+    # read header information
+    header_info = np.loadtxt( headerfile, dtype='|S20')
+    ncols       = np.int(header_info[0,1])
+    nrows       = np.int(header_info[1,1])
+    xllcorner   = np.float(header_info[2,1])
+    yllcorner   = np.float(header_info[3,1])
+    cs          = np.float(header_info[4,1])
+    missVal     = header_info[5,1]
+
+    # create x and y grid
+    xx          = np.linspace( xllcorner + cs/2,                xllcorner + cs/2 + (ncols-1)*cs, ncols)
+    yy          = np.linspace( yllcorner + cs/2 + (nrows-1)*cs, yllcorner + cs/2,                nrows)
+    xx, yy      = np.meshgrid(xx,yy)
+
+    #
+    # determine latitude and longitude of the Aimgrid
+    if coord_sys == '':
+        lons = xx
+        lats = yy
+    else:
+        projAim    = Proj(init=coord_sys)
+        lons, lats = projAim(xx, yy, inverse=True)
+
+    return lons, lats, xx, yy, missVal
+
+
+def latlon_to_nc(fhandle, lons, lats, xx, yy, missVal, suffix):
+    # This function writes the latitude longitude to file
+    varAtt  = ([['axis'     , 'X']])
+    varName = 'xc' + suffix[0]
+    dims    = xx.shape[1]
+    var     = xx[0,:] #np.arange(ncols)+1
+    writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, isdim=True)
+    #
+    varAtt  = ([['axis'     , 'Y']])
+    varName = 'yc' + suffix[0]
+    dims    = yy.shape[0]
+    var     = yy[:,0] #np.arange(nrows)+1
+    writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, isdim=True)
+    # Variables
+    # lon
+    varAtt  = ([['units'        , 'degrees_east'],
+               ['long_name'     , 'longitude' + suffix[1]],
+               ['missing_value' , missVal]])
+    #
+    varName = 'lon' + suffix[0]
+    var     = lons
+    dims    = ['yc' + suffix[0], 'xc' + suffix[0]]
+    writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, vartype='f8', comp=True)
+    # lat
+    varAtt  = ([['units'        , 'degrees_north'],
+               ['long_name'     , 'latitude' + suffix[1]],
+               ['missing_value' , missVal]])
+    varName = 'lat' + suffix[0]
+    var     = lats
+    dims    = ['yc' + suffix[0],'xc' + suffix[0]]
+    writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, vartype='f8', comp=True)
+
+    
 # COORDINATE SYSTEM
 #   if empty string is given, no coordinate transformation will be transformed
 #   code 31463 defines GK (DHDN3 Zone 3)
@@ -23,7 +90,8 @@ coord_sys = 'epsg:31467'
 #   specifies the grid properties
 #   for example, use a copy of the header.txt
 #   and adapt cellsize, ncols, nrows to your hydrologic resolution
-headerfile = 'header.txt'
+headerfile_l1 = ''
+headerfile_l0 = ''
 
 # OUTPUT FILE
 #   path to the output file, latlon.nc is hard-coded in mHM
@@ -43,18 +111,26 @@ parser = optparse.OptionParser(usage='%prog [options]',
 parser.add_option('-c', '--coord_sys', action='store', dest='coord_sys', type='string',
                   default=coord_sys, metavar='Property',
                   help='Coordinate system specifier according to http://www.spatialreference.org. (default: epsg:31467)')
-parser.add_option('-f', '--header', action='store', dest='headerfile', type='string',
-                  default=headerfile, metavar='Header file',
+parser.add_option('-f', '--header_l0', action='store', dest='headerfile_l0', type='string',
+                  default=headerfile_l0, metavar='Header file for level 0',
                   help='Header file containing information about e.g. number of rows and columns. (default: header.txt).')
+parser.add_option('-g', '--header_l1', action='store', dest='headerfile_l1', type='string',
+                  default=headerfile_l1, metavar='Header file for level 1',
+                  help='Header file containing information about e.g. number of rows and columns at level 1. (default: header.txt).')
 parser.add_option('-o', '--outfile', action='store', dest='outfile', type='string',
                   default=outfile, metavar='NetCDF file',
                   help='Name of NetCDF file. (default: latlon.nc).')
 
 (opts, args) = parser.parse_args()
 
-headerfile         = opts.headerfile
-outfile            = opts.outfile   
-coord_sys          = opts.coord_sys 
+headerfile_l1 = opts.headerfile_l1
+headerfile_l0 = opts.headerfile_l0
+outfile       = opts.outfile   
+coord_sys     = opts.coord_sys
+
+# check whether any headerfiles are given
+if headerfile_l0 == '' and headerfile_l1 == '':
+    raise ValueError('***ERROR: no headerfile specified, use -h switch for more information')
 
 #############################################
 
@@ -64,73 +140,29 @@ import time, os, sys                     # call current time for timestamp
 from pyproj import Proj
 from ufz    import writenetcdf      # from ufz
 
-# check input files
-
-if not os.path.isfile(headerfile):
-  sys.exit("No "+headerfile+" file found here, are you in the right directory?")
-
-# read header information
-header_info = np.loadtxt( headerfile, dtype='|S20')
-ncols       = np.int(header_info[0,1])
-nrows       = np.int(header_info[1,1])
-xllcorner   = np.float(header_info[2,1])
-yllcorner   = np.float(header_info[3,1])
-cs          = np.float(header_info[4,1])
-missVal     = header_info[5,1]
-
-# create x and y grid
-xx          = np.linspace( xllcorner + cs/2,                xllcorner + cs/2 + (ncols-1)*cs, ncols)
-yy          = np.linspace( yllcorner + cs/2 + (nrows-1)*cs, yllcorner + cs/2,                nrows)
-xx, yy      = np.meshgrid(xx,yy)
-
-#
-# determine latitude and longitude of the Aimgrid
-if coord_sys == '':
-    lons = xx
-    lats = yy
-else:
-    projAim    = Proj(init=coord_sys)
-    lons, lats = projAim(xx, yy, inverse=True)
-#
-# write netCDF
+# create netCDF Dataset
 fName   = outfile
 fhandle = nc.Dataset(fName, 'w', format='NETCDF4')
-#
+# add file attributes
 FiAtt  = ([['description', 'lat lon file'],
+           ['projection', coord_sys],
            ['history','Created ' + time.ctime(time.time()) ]])
 writenetcdf(fhandle, fileattributes=FiAtt)
+
+if headerfile_l0 != '':
+    # get lat lon for level 0 header file
+    lons, lats, xx, yy, missVal = header_to_latlon(headerfile_l0, coord_sys)
+    suffix = ['_l0', ' at level 0']
+    # write lat and lon for level 1 to file
+    latlon_to_nc(fhandle, lons, lats, xx, yy, missVal, suffix)
 #
-varAtt  = ([['axis'     , 'X']])
-varName = 'xc'
-dims    = ncols
-var     = xx[0,:] #np.arange(ncols)+1
-writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, isdim=True)
+if headerfile_l1 != '':
+    # get lat lon for level 1 header file
+    lons, lats, xx, yy, missVal = header_to_latlon(headerfile_l1, coord_sys)
+    suffix = ['_l1', ' at level 1']
+    # write lat and lon for level 1 to file
+    latlon_to_nc(fhandle, lons, lats, xx, yy, missVal, suffix)
 #
-varAtt  = ([['axis'     , 'Y']])
-varName = 'yc'
-dims    = nrows
-var     = yy[:,0] #np.arange(nrows)+1
-writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, isdim=True)
-# Variables
-# lon
-varAtt  = ([['units'        , 'degrees_east'],
-           ['long_name'     , 'longitude'],
-           ['missing_value' , missVal],
-           ['fill_value'    , missVal]])
-#
-varName = 'lon'
-var     = lons
-dims    = ['yc','xc']
-writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, vartype='f8', comp=True)
-# lat
-varAtt  = ([['units'        , 'degrees_north'],
-           ['long_name'     , 'latitude'],
-           ['missing_value' , missVal],
-           ['fill_value'    , missVal]])
-varName = 'lat'
-var     = lats
-dims    = ['yc','xc']
-writenetcdf(fhandle, name=varName, dims=dims, var=var, attributes=varAtt, vartype='f8', comp=True)
-#
+# close netcdf dataset
 fhandle.close()
 print outfile + " created."
