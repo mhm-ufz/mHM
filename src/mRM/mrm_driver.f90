b27902524c03678447fe8d4187dbbbee241e9015
Stephan Thober <stephan.thober@ufz.de>
2015-11-20 14:05:58 +0000

    trunk: corrected call of optimization subroutine in mRM stand-alone version

--- src/mRM/mrm_driver.f90
+++ src/mRM/mrm_driver.f90
@@ -10,7 +10,7 @@ program mrm_driver
   use mo_mrm_init,                      only: mrm_init
   use mo_mrm_objective_function_runoff, only: single_objective_runoff
   use mo_mrm_write,                     only: mrm_write, mrm_write_optifile, mrm_write_optinamelist
-  use mo_optimization,                  only: optimization_single
+  use mo_optimization,                  only: optimization
   use mo_timer,                         only: timers_init
 
   
@@ -31,7 +31,7 @@ program mrm_driver
      ! -----------------------------------------------------------------------
      ! OPTIMIZE
      ! -----------------------------------------------------------------------
-     call optimization_single(single_objective_runoff, dirConfigOut, funcbest, maskpara)
+     call optimization(single_objective_runoff, dirConfigOut, funcbest, maskpara)
      ! write a file with final objective function and the best parameter set
      call mrm_write_optifile(funcbest, global_parameters(:,3), global_parameters_name(:))
      ! write a file with final best parameter set in a namelist format
