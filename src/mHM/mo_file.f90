4271fa310557b34932f571900b5d2c23408986ea
Matthias Zink <matthias.zink@ufz.de>
2016-11-30 15:56:53 +0000

    trunk: polished docu, deleted TODO -> view Tickets in trac, set version number to 5.6

--- src/mHM/mo_file.f90
+++ src/mHM/mo_file.f90
@@ -12,9 +12,9 @@ MODULE mo_file
   IMPLICIT NONE
   
   !> Current mHM model version
-  CHARACTER(len=*), PARAMETER :: version                = '5.5'                         ! Version
+  CHARACTER(len=*), PARAMETER :: version                = '5.6'                         ! Version
   !> Time of current mHM model version release
-  CHARACTER(len=*), PARAMETER :: version_date           = 'Jun 2016'                   ! Release date
+  CHARACTER(len=*), PARAMETER :: version_date           = 'Dec 2016'                    ! Release date
   !> Driver file
   CHARACTER(len=*), PARAMETER :: file_main              = 'mhm_driver.f90'              ! Driver
   !> Namelist file name
