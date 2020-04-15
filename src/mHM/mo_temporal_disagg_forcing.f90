32d74c6072d73baa8a37ab219bdd67570554d752
MuellerSeb <mueller.seb@posteo.de>
2020-04-15 22:42:28 +0200

    disagg_forcing: separate routine for temperature

--- src/mHM/mo_temporal_disagg_forcing.f90
+++ src/mHM/mo_temporal_disagg_forcing.f90
@@ -20,6 +20,7 @@ MODULE mo_temporal_disagg_forcing
   PRIVATE
 
   PUBLIC :: temporal_disagg_forcing ! Temporally distribute forcings onto time step
+  PUBLIC :: temporal_disagg_temperature ! Temporally distribute temperature onto time step
 
   ! ------------------------------------------------------------------
 
@@ -70,76 +71,66 @@ CONTAINS
   ! S. Thober Jan 2017 - > added disaggregation based on weights given in nc file
   ! Robert Schweppe Jun 2018 - refactoring and reformatting
 
-  elemental pure subroutine temporal_disagg_forcing(isday, ntimesteps_day, prec_day, pet_day, temp_day, fday_prec, &
-                                                   fday_pet, fday_temp, fnight_prec, fnight_pet, fnight_temp, &
-                                                   temp_weights, pet_weights, pre_weights, read_meteo_weights, prec, &
-                                                   pet, temp)
+  elemental pure subroutine temporal_disagg_forcing( &
+      isday, ntimesteps_day, &
+      prec_day, pet_day, temp_day, &
+      fday_prec, fday_pet, fday_temp, &
+      fnight_prec, fnight_pet, fnight_temp, &
+      temp_weights, pet_weights, pre_weights, read_meteo_weights, &
+      prec, pet, temp &
+  )
     implicit none
 
     ! is day or night
     logical, intent(in) :: isday
-
     ! # of time steps per day
     real(dp), intent(in) :: ntimesteps_day
-
     ! Daily mean precipitation [mm/s]
     real(dp), intent(in) :: prec_day
-
     ! Daily mean ET [mm/s]
     real(dp), intent(in) :: pet_day
-
     ! Daily mean air temperature [K]
     real(dp), intent(in) :: temp_day
-
     ! Daytime fraction of precipitation
     real(dp), intent(in) :: fday_prec
-
     ! Daytime fraction of PET
     real(dp), intent(in) :: fday_pet
-
     ! Daytime air temparture increase
     real(dp), intent(in) :: fday_temp
-
     ! Daytime fraction of precipitation
     real(dp), intent(in) :: fnight_prec
-
     ! Daytime fraction of PET
     real(dp), intent(in) :: fnight_pet
-
     ! Daytime air temparture increase
     real(dp), intent(in) :: fnight_temp
-
     ! weights for average temperature
     real(dp), intent(in) :: temp_weights
-
     ! weights for PET
     real(dp), intent(in) :: pet_weights
-
     ! weights for precipitation
     real(dp), intent(in) :: pre_weights
-
     ! flag indicating that weights should be used
     logical, intent(in) :: read_meteo_weights
-
     ! Actual precipitation [mm/s]
     real(dp), intent(out) :: prec
-
     ! Reference ET [mm/s]
     real(dp), intent(out) :: pet
-
     ! Air temperature [K]
     real(dp), intent(out) :: temp
-
 
     ! default vaule used if ntimesteps_day = 1 (i.e., e.g. daily values)
     prec = prec_day
     pet = pet_day
-    temp = temp_day
-
+    ! use separate disaggregation subroutine for temperature
+    call temporal_disagg_temperature(&
+        isday, ntimesteps_day, &
+        temp_day, fday_temp, fnight_temp, &
+        temp_weights, read_meteo_weights, &
+        temp &
+    )
     if (read_meteo_weights) then
       ! apply weights
       pet = pet * pet_weights
-      temp = (temp + 273.15_dp) * temp_weights - 273.15 ! temperature weights are in K
       prec = prec * pre_weights
     else
       ! Distribute Prec, PET and Temp into time steps night/day
@@ -147,15 +138,58 @@ CONTAINS
         if (isday) then ! DAY-TIME
           prec = 2.0_dp * prec_day * fday_prec / ntimesteps_day
           pet = 2.0_dp * pet_day * fday_pet / ntimesteps_day
-          temp = temp_day + fday_temp
         else            ! NIGHT-TIME
           prec = 2.0_dp * prec_day * fnight_prec / ntimesteps_day
           pet = 2.0_dp * pet_day * fnight_pet / ntimesteps_day
-          temp = temp_day + fnight_temp
         end if
       end if
     end if
 
   end subroutine temporal_disagg_forcing
+
+  elemental pure subroutine temporal_disagg_temperature(&
+      isday, ntimesteps_day, &
+      temp_day, fday_temp, fnight_temp, &
+      temp_weights, read_meteo_weights, &
+      temp &
+  )
+    implicit none
+
+    ! is day or night
+    logical, intent(in) :: isday
+    ! # of time steps per day
+    real(dp), intent(in) :: ntimesteps_day
+    ! Daily mean air temperature [K]
+    real(dp), intent(in) :: temp_day
+    ! Daytime air temparture increase
+    real(dp), intent(in) :: fday_temp
+    ! Daytime air temparture increase
+    real(dp), intent(in) :: fnight_temp
+    ! weights for average temperature
+    real(dp), intent(in) :: temp_weights
+    ! flag indicating that weights should be used
+    logical, intent(in) :: read_meteo_weights
+    ! Air temperature [K]
+    real(dp), intent(out) :: temp
+
+
+    ! default vaule used if ntimesteps_day = 1 (i.e., e.g. daily values)
+    temp = temp_day
+
+    if (read_meteo_weights) then
+      ! apply weights
+      temp = (temp + 273.15_dp) * temp_weights - 273.15 ! temperature weights are in K
+    else
+      ! Distribute Temp into time steps night/day
+      if(ntimesteps_day .gt. 1.0_dp) then
+        if (isday) then ! DAY-TIME
+          temp = temp_day + fday_temp
+        else            ! NIGHT-TIME
+          temp = temp_day + fnight_temp
+        end if
+      end if
+    end if
+
+  end subroutine temporal_disagg_temperature
 
 END MODULE mo_temporal_disagg_forcing
