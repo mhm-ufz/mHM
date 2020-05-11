db84a2ec090aaa0b40852d1ed5d5d724135eec8e
muellese <sebastian.mueller@ufz.de>
2020-05-11 14:14:14 +0200

    RivTemp: add base class for riv temp module

---
+++ src/mRM/mo_mrm_riv_temp_class.f90
@@ -0,0 +1,93 @@
+!>       \file mo_mrm_riv_temp_class.f90
+
+!>       \brief Class for the river temperature calculations
+!>       \details
+
+!>       \authors Sebastian Mueller
+
+!>       \date Apr 2020
+
+module mo_mrm_riv_temp_class
+
+  use mo_kind, only: dp
+
+  implicit none
+
+  private
+  public :: riv_temp_type
+
+  type riv_temp_type
+    ! This is a container to define the river temperature routing in the current time step
+    character(256), dimension(:), allocatable :: dirWidths ! Directory where river widths are stored
+    character(256) :: riv_widths_file ! file name for river widths
+    character(256) :: riv_widths_name ! variable name for river widths
+    real(dp), public, dimension(:), allocatable :: L11_riv_widths !river widths in L11
+    real(dp) :: albedo_water ! albedo of open water
+    real(dp) :: pt_a_water ! priestley taylor alpha parameter for PET on open water
+    ! \f$ E_L \f$ Generated lateral temperature energy flux [m3 s-1 K]
+    real(dp), dimension(:), allocatable :: L1_lateral_E
+    real(dp), dimension(:, :), allocatable :: mRM_river_temp ! variable containing river temp for each domain and gauge
+  contains
+    procedure :: calc_lateral_E => pro_lateral_E
+  end type riv_temp_type
+
+contains
+
+  subroutine pro_lateral_E( &
+    self, &
+    fSealed_area_fraction, &
+    fast_interflow, &
+    slow_interflow, &
+    baseflow, &
+    direct_runoff, &
+    temp_air, &
+    mean_temp_air, &
+    add &
+    )
+
+    implicit none
+
+    class(riv_temp_type), intent(inout) :: self
+    ! sealed area fraction [1]
+    real(dp), dimension(:), intent(in) :: fSealed_area_fraction
+    ! \f$ q_0 \f$ Fast runoff component [mm tst-1]
+    real(dp), dimension(:), intent(in) :: fast_interflow
+    ! \f$ q_1 \f$ Slow runoff component [mm tst-1]
+    real(dp), dimension(:), intent(in) :: slow_interflow
+    ! \f$ q_2 \f$ Baseflow [mm tsts-1]
+    real(dp), dimension(:), intent(in) :: baseflow
+    ! \f$ q_D \f$ Direct runoff from impervious areas  [mm tst-1]
+    real(dp), dimension(:), intent(in) :: direct_runoff
+    ! air temperature [K]
+    real(dp), dimension(:), intent(in) :: temp_air
+    ! annual mean air temperature [K]
+    real(dp), dimension(:), intent(in) :: mean_temp_air
+    ! switch to turn on adding energy if routing TS is larger then mhm TS
+    logical, intent(in), optional :: add
+
+    ! internal switch for adding energy
+    logical :: do_add
+    ! lateral energy
+    real(dp), dimension(size(baseflow)) :: lateral_E
+
+    ! check optional argument add
+    if (present(add)) then
+        do_add = add
+    else
+        do_add = .false.
+    end if
+
+    lateral_E = ( &
+        (baseflow * mean_temp_air + (slow_interflow + fast_interflow) * temp_air) * (1.0_dp - fSealed_area_fraction) &
+        + direct_runoff * temp_air * fSealed_area_fraction &
+    )
+
+    if ( do_add ) then
+        self%L1_lateral_E = self%L1_lateral_E + lateral_E
+    else
+        self%L1_lateral_E = lateral_E
+    end if
+
+  end subroutine pro_lateral_E
+
+end module mo_mrm_riv_temp_class
