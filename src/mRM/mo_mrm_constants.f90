ad49ead0407709367e0f2658ef949a0c4237dc9c
muellese <sebastian.mueller@ufz.de>
2020-05-25 10:39:32 +0200

    riv-temp: add output flux state for riv-temp in mRM

--- src/mRM/mo_mrm_constants.f90
+++ src/mRM/mo_mrm_constants.f90
@@ -16,7 +16,7 @@ module mo_mrm_constants
   implicit none
   ! maximum number of outputs (fluxes states) for mrM
   ! TODO-RIV-TEMP: increase this to 2 (riv temp as second state)
-  integer(i4), public, parameter :: nOutFlxState = 1_i4     ! max. number of outputs to write into a netcdf file
+  integer(i4), public, parameter :: nOutFlxState = 2_i4     ! max. number of outputs to write into a netcdf file
   ! computational
   integer(i4), public, parameter :: nRoutingStates = 2 ! Dimension of the auxiliary vectors
   !                                                    ! which store current and past states of
