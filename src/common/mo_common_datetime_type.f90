142bacdad25eca10b31612e0d477b2fafe7a6ef6
Robert Schweppe <robert.schweppe@ufz.de>
2021-06-10 16:15:13 +0200

    [skip_ci] - rolled back update of yID through increment

--- src/common/mo_common_datetime_type.f90
+++ src/common/mo_common_datetime_type.f90
@@ -129,10 +129,9 @@ MODULE mo_common_datetime_type
     this%tIndex_out = 1 ! tt if write out of warming period
   end subroutine datetimeinfo_init
 
-  subroutine datetimeinfo_increment(this, iDomain)
+  subroutine datetimeinfo_increment(this)
     use mo_julian, only : caldat, julday
     class(datetimeinfo), intent(inout) :: this
-    integer(i4),     intent(in)    :: iDomain
 
     ! prepare the date and time information for next iteration step...
     ! set the current year as previous
@@ -154,15 +153,6 @@ MODULE mo_common_datetime_type
     if (this%prev_day   /= this%day) this%is_new_day = .true.
     if (this%prev_month /= this%month) this%is_new_month = .true.
     if (this%prev_year  /= this%year) this%is_new_year = .true.
-    ! second conditional is hack
-    ! increment is called after simulation to produce follow-up date to label time in netcdf files
-    ! however, this can mean, we are out of defined LCyearIds
-    ! in that case simply not alter the Id
-    ! TODO: still, it is a serious design flaw and should be adressed
-    if (this%is_new_year .and. ubound(LCyearId(:, iDomain), dim=1) >= this%year) then
-      ! update the yId index as well
-      this%yId  = LCyearId(this%year, iDomain)
-    end if
   end subroutine datetimeinfo_increment
 
   subroutine datetimeinfo_update_LAI_timestep(this)
