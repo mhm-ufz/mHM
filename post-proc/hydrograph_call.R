bb6211a052566e8c1fa1da560599bb5410bd9953
Pallav Kumar Shrestha <pallav-kumar.shrestha@ufz.de>
2022-03-22 14:38:41 +0100

    hydrograph.R can now plot hydrograph from both discharge.nc and daily_discharge.out

--- post-proc/hydrograph_call.R
+++ post-proc/hydrograph_call.R
@@ -29,11 +29,11 @@ source("hydrograph.R")
 ## Variables
 path <- "." # path to the discharge.nc file. 
 station_name <- "The Interesting Gauge" # Name of the station/ gauge
-station_id <- "12345" # Station ID (should be same as in mhm.nml, do not worry about the padded zeroes)
+station_id <- "398" # Station ID (should be same as in mhm.nml, do not worry about the padded zeroes)
 
 ## Generate texts
 title_text <- paste("Gauge: ", station_name, " . ", station_id) # Plot title 
   
 ## Call the hydrograph function 
-plot_hydrograph( path, station_id,  title_text )
+plot_hydrograph( path, station_id, title_text, "txt" ) # txt - read from daily_dicharge.txt, nc - read from discharge.nc
 
