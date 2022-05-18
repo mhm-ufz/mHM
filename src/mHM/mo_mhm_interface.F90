293be67bc24e053c75c9d1a7e46daf620e830b88
muellese <sebastian.mueller@ufz.de>
2022-05-18 17:33:18 +0200

    cleanup: add cleanup routines and call them in finalize routine

--- src/mHM/mo_mhm_interface.F90
+++ src/mHM/mo_mhm_interface.F90
@@ -434,6 +434,8 @@ contains
     use mo_restart, only: write_restart_files
     use mo_mrm_write, only : mrm_write
     use mo_mhm_messages, only: finish_message
+    use mo_clean_up, only: deallocate_global_variables
+    use mo_common_run_variables, only: run_cfg
 
     implicit none
 
@@ -471,6 +473,10 @@ contains
 #endif
 
     call finish_message()
+
+    ! clean up all allocated variables
+    call deallocate_global_variables()
+    call run_cfg%clean_up()
 
   end subroutine mhm_interface_finalize
 
