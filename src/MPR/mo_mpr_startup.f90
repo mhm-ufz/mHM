f03e7f6b9bbcb43578e535f7e1b8302da6223b26
muellese <sebastian.mueller@ufz.de>
2023-03-07 11:48:30 +0100

    mo_mpr_startup: docstring update

--- src/MPR/mo_mpr_startup.f90
+++ src/MPR/mo_mpr_startup.f90
@@ -12,10 +12,6 @@
 !> \ingroup f_mpr
 MODULE mo_mpr_startup
 
-  ! This module provides the startup routines for mHM.
-
-  ! Written Luis Samaniego, Rohini Kumar, Dec 2012
-
   USE mo_kind, ONLY : i4, dp
   use mo_common_constants, only : nodata_i4, nodata_dp   ! global nodata values (i4, dp)
 
@@ -27,45 +23,44 @@ MODULE mo_mpr_startup
 
 CONTAINS
 
-  ! ------------------------------------------------------------------
-
-  !    NAME
-  !        mpr_initialize
-
-  !    PURPOSE
-  !>       \brief Initialize main mHM variables
-
-  !>       \details Initialize main mHM variables for a given domain.
-  !>       Calls the following procedures in this order:
-  !>       - Constant initialization.
-  !>       - Generate soil database.
-  !>       - Checking inconsistencies input fields.
-  !>       - Variable initialization at level-0.
-  !>       - Variable initialization at level-1.
-  !>       - Variable initialization at level-11.
-  !>       - Space allocation of remaining variable/parameters.
-  !>       Global variables will be used at this stage.
-
-  !    HISTORY
-  !>       \authors Luis Samaniego, Rohini Kumar
-
-  !>       \date Dec 2012
-
-  ! Modifications:
-  ! Luis Samaniego Mar 2008 - fully distributed multilayer
-  ! Rohini Kumar   Oct 2010 - matrix to vector version
-  !                         - openmp parallelization
-  !                         - routing level 11
-  ! Luis Samaniego Jul 2012 - removal of IMSL dependencies
-  ! Luis Samaniego Dec 2012 - modular version
-  ! Rohini Kumar   May 2013 - code cleaned and error checks
-  ! Rohini Kumar   Nov 2013 - updated documentation
-  ! Stephan Thober Jun 2014 - copied L2 initialization from mo_meteo_forcings
-  ! Stephan Thober Jun 2014 - updated flag for read_restart
-  ! Stephan Thober Aug 2015 - removed initialisation of routing
-  ! Rohini Kumar   Mar 2016 - changes for handling multiple soil database options
-  ! Robert Schweppe Jun 2018 - refactoring and reformatting
-
+  !> \brief Initialize main mHM variables
+  !> \details Initialize main mHM variables for a given domain.
+  !! Calls the following procedures in this order:
+  !! - Constant initialization.
+  !! - Generate soil database.
+  !! - Checking inconsistencies input fields.
+  !! - Variable initialization at level-0.
+  !! - Variable initialization at level-1.
+  !! - Variable initialization at level-11.
+  !! - Space allocation of remaining variable/parameters.
+  !! Global variables will be used at this stage.
+  !> \changelog
+  !! - Luis Samaniego Mar 2008
+  !!   - fully distributed multilayer
+  !! - Rohini Kumar   Oct 2010
+  !!   - matrix to vector version
+  !!   - openmp parallelization
+  !!   - routing level 11
+  !! - Luis Samaniego Jul 2012
+  !!   - removal of IMSL dependencies
+  !! - Luis Samaniego Dec 2012
+  !!   - modular version
+  !! - Rohini Kumar   May 2013
+  !!   - code cleaned and error checks
+  !! - Rohini Kumar   Nov 2013
+  !!   - updated documentation
+  !! - Stephan Thober Jun 2014
+  !!   - copied L2 initialization from mo_meteo_forcings
+  !! - Stephan Thober Jun 2014
+  !!   - updated flag for read_restart
+  !! - Stephan Thober Aug 2015
+  !!   - removed initialisation of routing
+  !! - Rohini Kumar   Mar 2016
+  !!   - changes for handling multiple soil database options
+  !! - Robert Schweppe Jun 2018
+  !!   - refactoring and reformatting
+  !> \authors Luis Samaniego, Rohini Kumar
+  !> \date Dec 2012
   subroutine mpr_initialize
 
     use mo_common_variables, only : l0_l1_remap, level0, level1, domainMeta, resolutionHydrology
@@ -113,31 +108,21 @@ CONTAINS
   end subroutine mpr_initialize
 
 
-  ! ------------------------------------------------------------------
-
-  !    NAME
-  !        L0_check_input
-
-  !    PURPOSE
-  !>       \brief Check for errors in L0 input data
-
-  !>       \details Check for possible errors in input data (morphological and land cover) at level-0
-
-  !    INTENT(IN)
-  !>       \param[in] "integer(i4) :: iDomain" domain id
-
-  !    HISTORY
-  !>       \authors Rohini Kumar
-
-  !>       \date Jan 2013
-
-  ! Modifications:
-  ! Rohini Kumar   Aug  2013 - added iFlag_LAI_data_format to handle LAI options, and changed within the code made accordingly
-  ! Rohini  Kumar  Sep 2013 - read input data for routing processes according & Stephan Thober,           to process_matrix flag
-  ! Stephan Thober Aug 2015 - moved check of L0 routing variables to mRM
-  ! Rohini Kumar   Mar 2016 - changes for handling multiple soil database options
-  ! Robert Schweppe Jun 2018 - refactoring and reformatting
-
+  !> \brief Check for errors in L0 input data
+  !> \details Check for possible errors in input data (morphological and land cover) at level-0
+  !> \changelog
+  !! - Rohini Kumar   Aug  2013
+  !!   - added iFlag_LAI_data_format to handle LAI options, and changed within the code made accordingly
+  !! - Rohini  Kumar  Sep 2013
+  !!   - read input data for routing processes according & Stephan Thober, to process_matrix flag
+  !! - Stephan Thober Aug 2015
+  !!   - moved check of L0 routing variables to mRM
+  !! - Rohini Kumar   Mar 2016
+  !!   - changes for handling multiple soil database options
+  !! - Robert Schweppe Jun 2018
+  !!   - refactoring and reformatting
+  !> \authors Rohini Kumar
+  !> \date Jan 2013
   subroutine L0_check_input(iDomain)
 
     use mo_common_constants, only : eps_dp
@@ -150,7 +135,7 @@ CONTAINS
 
     implicit none
 
-    ! domain id
+    !> domain id
     integer(i4), intent(in) :: iDomain
 
     integer(i4) :: k, n, nH
@@ -222,43 +207,33 @@ CONTAINS
 
   end subroutine L0_check_input
 
-  ! ------------------------------------------------------------------
 
-  !    NAME
-  !        L0_variable_init
-
-  !    PURPOSE
-  !>       \brief level 0 variable initialization
-
-  !>       \details following tasks are performed for L0 data sets
-  !>       -  cell id & numbering
-  !>       -  storage of cell cordinates (row and coloum id)
-  !>       -  empirical dist. of terrain slope
-  !>       -  flag to determine the presence of a particular soil id
-  !>       in this configuration of the model run
-  !>       If a variable is added or removed here, then it also has to
-  !>       be added or removed in the subroutine config_variables_set in
-  !>       module mo_restart and in the subroutine set_config in module
-  !>       mo_set_netcdf_restart
-
-  !    INTENT(IN)
-  !>       \param[in] "integer(i4) :: iDomain" domain id
-
-  !    HISTORY
-  !>       \authors Rohini Kumar
-
-  !>       \date Jan 2013
-
-  ! Modifications:
-  ! Rohini Kumar & Matthias Cuntz  May 2014 - cell area calulation based on a regular lat-lon grid or
-  !                                           on a regular X-Y coordinate system
-  ! Matthias Cuntz                 May 2014 - changed empirical distribution function so that doubles get the same value
-  ! Matthias Zink & Matthias Cuntz Feb 2016 - code speed up due to reformulation of CDF calculation
-  ! Rohini Kumar                   Mar 2016 - changes for handling multiple soil database options
-  ! Maren Kaluza                   Feb 2018 - removed slope_val, temp, only sort the index to speed up
-  !                                           finding the empirical distribution slope_emp
-  ! Robert Schweppe                Jun 2018 - refactoring and reformatting
-
+  !> \brief level 0 variable initialization
+  !> \details following tasks are performed for L0 data sets
+  !! -  cell id & numbering
+  !! -  storage of cell cordinates (row and coloum id)
+  !! -  empirical dist. of terrain slope
+  !! -  flag to determine the presence of a particular soil id
+  !! in this configuration of the model run
+  !! If a variable is added or removed here, then it also has to
+  !! be added or removed in the subroutine config_variables_set in
+  !! module mo_restart and in the subroutine set_config in module
+  !! mo_set_netcdf_restart
+  !> \changelog
+  !! - Rohini Kumar & Matthias Cuntz  May 2014
+  !!   - cell area calulation based on a regular lat-lon grid or on a regular X-Y coordinate system
+  !! - Matthias Cuntz           May 2014
+  !!   - changed empirical distribution function so that doubles get the same value
+  !! - Matthias Zink & Matthias Cuntz Feb 2016
+  !!   - code speed up due to reformulation of CDF calculation
+  !! - Rohini Kumar             Mar 2016
+  !!   - changes for handling multiple soil database options
+  !! - Maren Kaluza             Feb 2018
+  !!   - removed slope_val, temp, only sort the index to speed up finding the empirical distribution slope_emp
+  !! - Robert Schweppe          Jun 2018
+  !!   - refactoring and reformatting
+  !> \authors Rohini Kumar
+  !> \date Jan 2013
   subroutine L0_variable_init(iDomain)
 
     use mo_append, only : append
@@ -271,7 +246,7 @@ CONTAINS
 
     implicit none
 
-    ! domain id
+    !> domain id
     integer(i4), intent(in) :: iDomain
 
     real(dp), dimension(:), allocatable :: slope_emp
@@ -361,36 +336,30 @@ CONTAINS
 
   end subroutine L0_variable_init
 
-  ! ------------------------------------------------------------------
 
-  !    NAME
-  !        init_eff_params
-
-  !    PURPOSE
-  !>       \brief Allocation of space for mHM related L1 and L11 variables.
-
-  !>       \details Allocation of space for mHM related L1 and L11 variables (e.g., states,
-  !>       fluxes, and parameters) for a given domain. Variables allocated here is
-  !>       defined in them mo_global_variables.f90 file. After allocating any variable
-  !>       in this routine, initalize them in the following variables_default_init
-  !>       subroutine:
-
-  !    INTENT(IN)
-  !>       \param[in] "integer(i4) :: ncells1"
-
-  !    HISTORY
-  !>       \authors Rohini Kumar
-
-  !>       \date Jan 2013
-
-  ! Modifications:
-  ! R. Kumar           Sep 2013 - documentation added according to the template
-  ! S. Thober          Aug 2015 - removed routing related variables
-  ! Zink M. Demirel C. Mar 2017 - Init Jarvis soil water stress variable at SM process(3)
-  ! Robert Schweppe    Dec 2017 - restructured allocation in variables_alloc, expanded dimensions of effective parameters
-  ! Robert Schweppe    Jun 2018 - refactoring and reformatting
-  ! Rohini Kumar       Oct 2021 - Added Neutron count module to mHM integrate into develop branch (5.11.2)
-
+  !> \brief Allocation of space for mHM related L1 and L11 variables.
+  !> \details Allocation of space for mHM related L1 and L11 variables (e.g., states,
+  !! fluxes, and parameters) for a given domain. Variables allocated here is
+  !! defined in them mo_global_variables.f90 file. After allocating any variable
+  !! in this routine, initalize them in the following variables_default_init
+  !! subroutine.
+  !> \changelog
+  !! - R. Kumar           Sep 2013
+  !!   - documentation added according to the template
+  !! - S. Thober          Aug 2015
+  !!   - removed routing related variables
+  !! - Zink M. Demirel C. Mar 2017
+  !!   - Init Jarvis soil water stress variable at SM process(3)
+  !! - Robert Schweppe    Dec 2017
+  !!   - restructured allocation in variables_alloc, expanded dimensions of effective parameters
+  !! - Robert Schweppe    Jun 2018
+  !!   - refactoring and reformatting
+  !! - Rohini Kumar       Oct 2021
+  !!   - Added Neutron count module to mHM integrate into develop branch (5.11.2)
+  !! - Sebastian Müller Mar 2023
+  !!   - made L1_alpha, L1_kSlowFlow, L1_kBaseFlow and L1_kPerco land cover dependent
+  !> \authors Rohini Kumar
+  !> \date Jan 2013
   subroutine init_eff_params(ncells1)
 
     use mo_append, only : append
@@ -407,6 +376,7 @@ CONTAINS
 
     implicit none
 
+    !> number of L1 cells
     integer(i4), intent(in) :: ncells1
 
     real(dp), dimension(:, :, :), allocatable :: dummy_3D
